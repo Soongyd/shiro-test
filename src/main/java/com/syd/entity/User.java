25d83e78086e866bd809b6214337a15d0ae3b200
songyd <897235330@qq.com>
2020-09-17 21:53:01 +0800

    git项目第二次 v1.0

---
+++ src/main/java/com/syd/entity/User.java
@@ -0,0 +1,13 @@
+package com.syd.entity;
+
+/**
+ * @ClassName User
+ * @Author song
+ * @Date 2020/9/17 21:50
+ * @Description DOOO
+ */
+public class User {
+    private String id;
+    private String username;
+    private String password;
+}
